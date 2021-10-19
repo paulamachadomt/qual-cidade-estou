201f68f297886b2e70891d7be0abcca2091d55ae
paulamachadomt <paulamachadomt@gmail.com>
2021-10-19 00:15:17 -0300

    geocode reverso ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,16 +11,19 @@
         <h2>Coordenadas</h2>
         <h3>{{latitude}}</h3>
         <h3>{{longitude}}</h3>
+        <ion-button @click="buscaLugar" >Qual cidade?</ion-button>
+        <h3>{{cidade}}</h3>
       </div>
     </ion-content>
 
   </ion-page>
 </template>
 
-<script lang="js">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { Geolocation } from '@capacitor/geolocation';
+import { Http } from '@capacitor-community/http';
 
 export default defineComponent({
   name: 'Home',
@@ -29,12 +32,14 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonButton
   },
   data() {
     return {
       latitude: 0,
-      longitude: 0
+      longitude: 0,
+      cidade: ''
     }
   },
   ionViewWillEnter() {
@@ -47,6 +52,19 @@ export default defineComponent({
 
       this.latitude = coordinates.coords.latitude;
       this.longitude = coordinates.coords.longitude;
+    },
+    buscaLugar: async function() {
+      const ACCESS_KEY = '5060464ccd39bf309c642f5d8e039907';
+      //const url = 'http://api.positionstack.com/v1/reverse?access_key=' + ACCESS_KEY + '&query=' + this.latitude + ',' + this.longitude;    
+
+      const options = {
+        url: `http://api.positionstack.com/v1/reverse?access_key=${ACCESS_KEY}&query=${this.latitude},${this.longitude}&limit=1`
+      };
+
+      const response = await Http.get(options);
+      console.log('======RESPONNNNSE', response);
+
+      this.cidade = response.data.data[0].locality + ', ' + response.data.data[0].region_code;
     }
   }
 });
